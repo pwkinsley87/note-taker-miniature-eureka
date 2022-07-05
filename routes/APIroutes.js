03e40569f612752e285c2a1a259b68ebd2a6d8c3
Patrick Kinsley <102868418+pwkinsley87@users.noreply.github.com>
2022-07-05 14:33:38 -0500

    added API routes

--- routes/APIroutes.js
+++ routes/APIroutes.js
@@ -1,4 +1,39 @@
 const express = require('express');
-const db = require('./db/connection');
+const db = require('../db/db.json');
 const apiRouters = require('./routes/APIroutes');
 const router = express.Router();
+const uniqid = require('uniqid');
+
+router.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', (err, data) => {
+        if (err) throw err;
+        console.log(JSON.parse(data));
+
+        res.send(data)
+    })
+})
+
+router.post('/api/notes', (req, res) => {
+    let newNote = {
+        id: uniqid(),
+        title: req.body.title,
+        text: req.body.text
+    }
+    fs.readFile('./db/db.json', (err, data) => {
+        if (err) throw err;
+
+        let newData = JSON.parse(data);
+
+        newData.push(newNote);
+        console.log(newData)
+
+        fs.writeFile('./db/db.json', JSON.stringify(newData), (err) => {
+            if (err) throw err;
+
+            res.send('Note successfully added!');
+        })
+    });
+})
+
+module.exports = router;
+
